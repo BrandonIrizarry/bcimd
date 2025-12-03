6755eb177cccde6c2d92ab87b6cae58d1be497e2
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2025-12-03 13:34:39 -0500

    Tentative final draft of logic
    So far so good

--- generate-toc.el
+++ generate-toc.el
@@ -13,51 +13,27 @@ An empty Table of Contents level-1 header must already exist."
     (save-excursion
       (goto-char toc)
 
-      (let (ids)
+      (let (objs)
         (while (re-search-forward "#[[:space:]]+\\([[:graph:]].*\\)" nil t)
           (let* ((header-start (match-beginning 0))
-                 (header-content (match-string-no-properties 1))
+                 (content (match-string-no-properties 1))
                  (id (mapconcat #'downcase
-                                (string-split header-content " ")
+                                (string-split content " ")
                                 "-")))
             (save-excursion
               (goto-char header-start)
               (forward-line -1)
               (insert (format "<a id=\"%s\"></a>" id)))
-            (push (cons header-content id) ids)))
-        (nreverse ids)))))
+            (push (cons content id) objs)))
 
+        (setq objs (nreverse objs))
 
-(defun test ()
-  (interactive)
+        ;; Insert the TOC entries!
+        (save-excursion
+          (goto-char toc)
+          (forward-line 2)
 
-  ;; For each heading, prepend an anchor element with the appropriate
-  ;; id attribute.
-  ;;
-  ;; Then save the ids in IDS.
-  (let (ids)
-    (save-excursion
-      (goto-char (point-min))
-
-      ;; Skip past the front matter section.
-      (while (re-search-forward "<a id=\"\\([[:graph:]-]+\\)\"></a>" nil t)
-        (push (match-string-no-properties 1) ids)))
-
-    ;; Make sure we have them in the correct order.
-    (setq ids (reverse ids))
-
-    ;; Insert the corresponding list item inside the Table of
-    ;; Contents.
-    (let ((pos (save-excursion
-                 (goto-char (point-min))
-                 (search-forward "# Table of Contents" nil t))))
-      (unless pos
-        (user-error "Missing '# Table of Contents' header"))
-
-      (goto-char pos)
-      (forward-line 2)
-
-      ;; Insert the TOC entries!
-      (dolist (id ids)
-        (let ((proper (mapconcat #'capitalize (string-split id "-") " ")))
-          (insert (format "+ [%s](#%s)\n" proper id)))))))
+          (dolist (obj objs)
+            (let ((content (car obj))
+                  (id (cdr obj)))
+              (insert (format "+ [%s](#%s)\n" content id)))))))))
